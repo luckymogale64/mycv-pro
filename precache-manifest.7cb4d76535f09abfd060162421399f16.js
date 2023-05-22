331bd35dc58f4743503595129e7ff921bc52d6fd
luckymogale64 <mogalel615@gmail.com>
2023-05-22 16:07:35 +0200

    Updates

---
+++ precache-manifest.7cb4d76535f09abfd060162421399f16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93aaa963838909823c6dff53fdc448e2",
+    "url": "/mycv-pro/index.html"
+  },
+  {
+    "revision": "5090c5aa5f38229a4599",
+    "url": "/mycv-pro/static/css/2.3758f9f2.chunk.css"
+  },
+  {
+    "revision": "afe232ad218271ee992b",
+    "url": "/mycv-pro/static/css/main.1bcb5c0e.chunk.css"
+  },
+  {
+    "revision": "5090c5aa5f38229a4599",
+    "url": "/mycv-pro/static/js/2.0206e306.chunk.js"
+  },
+  {
+    "revision": "afe232ad218271ee992b",
+    "url": "/mycv-pro/static/js/main.1eb9cd34.chunk.js"
+  },
+  {
+    "revision": "6adda5c89d5925d825f2",
+    "url": "/mycv-pro/static/js/runtime~main.0996d46e.js"
+  }
+]);
